d798da4cc74eb44826d723480953fa977b6ae662
James Crutchley <jamescrutchley98@gmail.com>
2023-07-14 14:06:55 +1000

    add logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import './App.css';
+import Overview from './components/Overview';
+import { Component } from 'react';
+import uniqid from "uniqid";
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: {
+                text: '',
+                id: uniqid(),
+            },
+            tasks: [],
+        }
+    }
+
+   addTask = (e) => {
+    e.preventDefault();
+
+    const { tasks, inputValue } = this.state;
+    this.setState({
+        tasks: [...tasks, inputValue],
+        inputValue: {
+            text: '',
+            id: uniqid(),
+        }
+    })
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ inputValue: {
+        text: event.target.value,
+        id: uniqid(),
+    }})
+}
+
+  render() {
+    const { tasks, inputValue} = this.state;
+    return (
+        <div className="App">
+            <form onSubmit={this.addTask}>
+                <label htmlFor="taskInput">Enter Task: </label>
+                <input id="taskInput" onChange={this.handleInputChange} type="text" value={inputValue.text}></input>
+                <button type="submit">Submit</button>
+            </form>
+            <Overview tasks={tasks}/>
+        </div>
+      );
+  }
+
 }
 
 export default App;
